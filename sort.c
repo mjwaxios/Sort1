a1116d6e544242dc369e56e9bf805c1b268adb0c
root <root@ADSB.n3uc.com>
2017-11-13 22:10:17 +0000

    First version but has a bug

---
+++ sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int size, int array[]) {
+	for(int i=0; i<size;i++) 
+        {
+		for(int j=0; j<i; j++)
+		{
+			if(array[j] > array[j+1])
+			{
+				int temp=array[j];
+				array[j]=array[j+1];
+				array[j+1]=temp;
+			}
+		}
+	
+	printf("%d ", array[i] );
+	}
+}
+
+void main (int argc, char *argv[])
+{
+	if(argc != 12)
+	{
+		printf("Please enter 10 integers and the number for your selected sort.");
+	}
+	else
+	{
+		int size = 10;
+		int arr[size];
+		for(int i=0;i<size;i++)
+		{
+			arr[i] = atoi(argv[i+1]);
+		}
+		if(atoi(argv[size+1]) == 1)
+		{
+			bubbleSort(size,arr);
+                        printf("\n");
+			for(int i=0; i < size; i++) printf("%d ", arr[i]);
+                        printf("\n");
+		}
+	}
+}
+
+
+
